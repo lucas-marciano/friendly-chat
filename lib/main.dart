e83b48666d87859f9610c35e30c337bc875b6e61
lucas-marciano <lucas.almeida.marciano@gmail.com>
2020-03-19 09:36:00 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Friendly Chat",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Friendlychat"),
+        ),
+      ),
+    ),
+  );
+}
